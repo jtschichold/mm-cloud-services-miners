1ef957b9b67cc64a12e7afc0e70b9c331d480335
Luigi Mori <lmori@paloaltonetworks.com>
2021-01-17 12:08:26 +0100

    New approach to tests

--- __tests__/adobe.test.ts
+++ __tests__/adobe.test.ts
@@ -1,7 +1,33 @@
-import * as adobe from '../src/miners/adobe'
+jest.mock('node-fetch', () => require('fetch-mock-jest').sandbox())
+const fetchMock = require('node-fetch')
+import * as fs from 'fs'
 
-test('test adobe', async () => {
+import * as adobe from '../src/miners/adobe'
+const {Response} = jest.requireActual('node-fetch')
+
+test('test AdobeCreativeMiner', async () => {
+    // mock setup
+    fetchMock.get(
+        'https://helpx.adobe.com/in/enterprise/kb/network-endpoints.html',
+        new Response(
+            fs.readFileSync(
+                './__tests__/data/AdobeCreativeMiner-network-endpoints.html',
+                {encoding: 'utf-8'}
+            ),
+            {
+                status: 200
+            }
+        )
+    )
+    const expected = JSON.parse(
+        fs.readFileSync('./__tests__/data/AdobeCreativeMiner-response.json', {
+            encoding: 'utf-8'
+        })
+    )
+
     const args = new Map()
 
     const result = await adobe.registry['AdobeCreativeMiner'].miner(args)
+
+    expect(result).toEqual(expected)
 })
