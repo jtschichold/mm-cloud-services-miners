e46dc799feca738aa74d4ef8f69e24345e55b7a8
Luigi Mori <lmori@paloaltonetworks.com>
2021-01-17 12:43:03 +0100

    Adds tests for o365 and azure

--- __tests__/azure.test.ts
+++ __tests__/azure.test.ts
@@ -1,8 +1,43 @@
-import * as azure from '../src/miners/azure'
+jest.mock('node-fetch', () => require('fetch-mock-jest').sandbox())
+const fetchMock = require('node-fetch')
+import * as fs from 'fs'
 
-test('test azure', async () => {
+import * as azure from '../src/miners/azure'
+const {Response} = jest.requireActual('node-fetch')
+
+test('test AzureWithServiceTagsMiner', async () => {
+    // setup mock
+    fetchMock.get(
+        'https://www.microsoft.com/en-us/download/confirmation.aspx?id=56519',
+        new Response(
+            '<html><body><a class="failoverLink" href="http://example.com/mockAzure.json"/></body></html>',
+            {
+                status: 200
+            }
+        )
+    )
+    fetchMock.get(
+        'http://example.com/mockAzure.json',
+        new Response(
+            fs.readFileSync(
+                './__tests__/data/AzureWithServiceTagsMiner-input.json',
+                {encoding: 'utf-8'}
+            ),
+            {
+                status: 200
+            }
+        )
+    )
+    const expected = JSON.parse(
+        fs.readFileSync('./__tests__/data/AzureWithServiceTagsMiner-result.json', {
+            encoding: 'utf-8'
+        })
+    )
+
     const args = new Map()
     args.set('cloud', 'PublicCloud')
 
     const result = await azure.registry['AzureWithServiceTagsMiner'].miner(args)
+
+    expect(result).toEqual(expected)
 })
--- __tests__/office365.test.ts
+++ __tests__/office365.test.ts
@@ -1,11 +1,39 @@
+jest.mock('node-fetch', () => require('fetch-mock-jest').sandbox())
+const fetchMock = require('node-fetch')
+import * as fs from 'fs'
+
 import * as office365 from '../src/miners/office365'
 import * as uuid from 'uuid'
+const {Response} = jest.requireActual('node-fetch')
 
-test('test office365', async () => {
+test('test O365Miner', async () => {
+    const clientGuid = uuid.v4()
+
+    // setup mock
+    process.env.O365MINER_CLIENT_GUID = clientGuid
+
+    fetchMock.get(
+        `https://endpoints.office.com/endpoints/Worldwide?ClientRequestId=${clientGuid}`,
+        new Response(
+            fs.readFileSync(
+                './__tests__/data/O365Miner-input.json',
+                {encoding: 'utf-8'}
+            ),
+            {
+                status: 200
+            }
+        )
+    )
+    const expected = JSON.parse(
+        fs.readFileSync('./__tests__/data/O365Miner-result.json', {
+            encoding: 'utf-8'
+        })
+    )
+
     const args = new Map()
     args.set('instance', 'Worldwide')
 
-    process.env.O365MINER_CLIENT_GUID = uuid.v4()
-
     const result = await office365.registry['O365Miner'].miner(args)
+
+    expect(result).toEqual(expected)
 })
